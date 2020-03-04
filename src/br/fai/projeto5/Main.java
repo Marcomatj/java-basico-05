188dec24bdff65c290334d99b2e14fc8a1e5871f
Matuk <mateusmatuk@fai-mg.br>
2020-03-04 20:27:04 -0300

    Segundo commit

--- src/br/fai/projeto5/Main.java
+++ src/br/fai/projeto5/Main.java
@@ -10,8 +10,27 @@ public class Main {
 
 	private void start() {
 		
+		int cont = 0;
 		
-		
+		for (cont = 0; cont <= 5; cont ++) {
+		switch(cont) {
+		case 1:
+			System.out.println("Primeira condicao.");
+			break;
+		case 2:
+			System.out.println("Nao encontrou uma condicao.");
+			break;
+		case 3:
+			System.out.println("Nao encontrou uma condicao.");
+			break;
+		case 4:
+			System.out.println("Segunda condicao");
+			break;
+		case 5:
+			System.out.println("Nao encontrou uma condicao.");
+			break;
+		}
+		}
 	}
 
 }
